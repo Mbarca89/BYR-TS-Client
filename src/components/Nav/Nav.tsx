0af9c81831e4a33e4e246f672a45379111d9a96e
Mbarca89 <mauriciobarca1989@gmail.com>
2023-07-24 21:48:36 +0200

    nac bug

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -26,46 +26,44 @@ const Nav = () => {
 
     console.log(location.pathname)
 
-    // if (mobile) return (
-    //     <div className={style.nav}>
-    //         <div className={style.logoContainer}>
-    //             <NavLink to='/'>
-    //                 <img className={style.logo} src={logo} alt='B&R Inmobiliaria' />
-    //             </NavLink>
-    //         </div>
-    //         <div className={style.menuContainer}>
-    //             <div className={style.socialContainer}>
-    //                 <div>
-    //                     <a className={style.social} href={`https://www.instagram.com/byrinmobiliaria/`} target="_blank" rel="noopener noreferrer">
-    //                         <img src={instagram} alt='B&R Inmobiliaria'></img>
-    //                     </a>
-    //                 </div>
-    //                 <div>
-    //                     <a className={style.social} href={`https://www.facebook.com/ByRdesarrollosinmobiliarios`} target="_blank" rel="noopener noreferrer">
-    //                         <img src={facebook} alt='B&R Inmobiliaria'></img>
-    //                     </a>
-    //                 </div>
-    //                 <div>
-    //                     <a className={style.social} href={`https://api.whatsapp.com/send?phone=5492664570187&text=Hola, Necesito asesoramiento sobre una propiedad`} target="_blank" rel="noopener noreferrer">
-    //                         <img src={whatsapp} alt='B&R Inmobiliaria'></img>
-    //                     </a>
-    //                 </div>
-    //             </div>
-    //             <div className={style.buttonContainer}>
-    //                 <NavLink to='/properties' className={style.button}>Propiedades</NavLink>
-    //                 {/* <NavLink to='/about' className={style.button}>Inversiones</NavLink> */}
-    //                 <NavLink to='/tasaciones' className={style.button}>Tasaciones</NavLink>
-    //                 <NavLink to='/empresa' className={style.button}>Empresa</NavLink>
-    //             </div>
-    //             <a className={style.developer} href={`https://github.com/Mbarca89`} target="_blank" rel="noopener noreferrer">Desarollado por Mauricio Barca</a>
-    //         </div>
-    //         <div className={style.mobileLine} />
-    //     </div>
-
-    // )
+    if (mobile) return (
+        <div className={style.header} style={location.pathname === '/properties' ? { backgroundColor: 'transparent', height: '20vh' } : undefined}>
+            <div className={style.menu} onClick={showNavHandler}>
+                {!showNav ?
+                    <GiHamburgerMenu size={35} color={'#4a4a4a'} style={{ marginTop: '3%', marginLeft: '3%', cursor: 'pointer' }} />
+                    : <GrFormClose size={35} color={'#4a4a4a'} style={{ marginTop: '3%', marginLeft: '3%', cursor: 'pointer' }} />}
+            </div>
+            <div className={style.logoContainer}>
+                    <NavLink to='/'>
+                        <img className={style.logo} src={logo} alt='B&R Inmobiliaria' />
+                    </NavLink>
+                </div>
+            <div className={style.socialContainer}>
+            <a className={style.social} href={`https://www.instagram.com/byrinmobiliaria/`} target="_blank" rel="noopener noreferrer">
+                <img src={instagram} alt='B&R Inmobiliaria'></img>
+            </a>
+            <a className={style.social} href={`https://www.facebook.com/ByRdesarrollosinmobiliarios`} target="_blank" rel="noopener noreferrer">
+                <img src={facebook} alt='B&R Inmobiliaria'></img>
+            </a>
+            <a className={style.social} href={`https://api.whatsapp.com/send?phone=5492664570187&text=Hola, Necesito asesoramiento sobre una propiedad`} target="_blank" rel="noopener noreferrer">
+                <img src={whatsapp} alt='B&R Inmobiliaria'></img>
+            </a>
+        </div>
+            <div className={showNav ? style.nav : style.noNav}>
+                <div className={style.line}></div>
+                <div className={style.buttonContainer} onClick={showNavHandler}>
+                    <NavLink to='/' className={style.button}>Inicio</NavLink>
+                    <NavLink to='/properties' className={style.button}>Propiedades</NavLink>
+                    <NavLink to='/tasaciones' className={style.button}>Tasaciones</NavLink>
+                    <NavLink to='/empresa' className={style.button}>Empresa</NavLink>
+                </div>
+                <div className={style.line} />
+            </div>
+        </div>
+    )
 
     return (
-        <div className={style.header} style={location.pathname === '/properties' ? { backgroundColor: 'transparent', height:'20vh' } : undefined}>
+        <div className={style.header} style={location.pathname === '/properties' ? { backgroundColor: 'transparent', height: '20vh' } : undefined}>
             <div className={style.menu} onClick={showNavHandler}>
                 {!showNav ?
                     <GiHamburgerMenu size={35} color={'#4a4a4a'} style={{ marginTop: '3%', marginLeft: '3%', cursor: 'pointer' }} />
